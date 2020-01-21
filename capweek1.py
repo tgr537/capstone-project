440064fbc776cf9d53377f61b315adef6e02b987
tgr537 <giridharreddyt537@gmail.com>
2020-01-21 17:33:47 +0530

    link for the notebook

---
+++ capweek1.py
@@ -0,0 +1,1 @@
+https://eu-gb.dataplatform.cloud.ibm.com/analytics/notebooks/v2/9403d0a9-f08f-45c6-9b03-4ee011c593fa/view?access_token=a818cdb8da6a333f7530f39df1770eb749959fe734acaa911c4d40b19f5c68biia
